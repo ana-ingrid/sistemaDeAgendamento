1fb9f9759007a496fe51d6b8483027b8155f41f5
ana-ingrid <ingridsantoscosta2003@gmail.com>
2024-01-23 16:21:42 -0300

    commit inicial

---
+++ src/test/java/com/br/agendamento/AgendamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.agendamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

310236a8e4ee43886320d4a307e8bb64ad1953be
ana-ingrid <ingridsantoscosta2003@gmail.com>
2024-09-18 12:45:23 -0300

    atualiza serviço ClienteService removendo métodos de teste

--- src/main/java/com/br/agendamento/service/ClienteService.java
+++ src/main/java/com/br/agendamento/service/ClienteService.java
@@ -1,27 +1,16 @@
 package com.br.agendamento.service;
 
 
-import com.br.agendamento.model.dto.ClienteDTO;
-import com.br.agendamento.repository.AgendamentoRepository;
-import com.br.agendamento.repository.ClienteRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.br.agendamento.repository.UsuarioRepository;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
+@AllArgsConstructor
+@NoArgsConstructor
 public class ClienteService {
 
-    @Autowired
-    protected AgendamentoRepository agendamentoRepository;
-
-    @Autowired
-    protected ClienteRepository clienteRepository;
-
-
-    public List<ClienteDTO> listaClientes() {
-        return clienteRepository.findAll().stream().map(ClienteDTO::new).toList();
-    }
-
+    private UsuarioRepository usuarioRepository;
 
 }
